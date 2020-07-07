372e31ce7db2a67968acbe5380d39b2c3cd2df1d
pechaprasanna <rpv31dec1991@gmail.com>
2020-07-07 10:29:22 -0700

    Updates

---
+++ precache-manifest.83422b87070a20a432ad01eaaac0b8ac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc77b4b6b820c894bc864c8fe8378ef5",
+    "url": "/robotfriendspwa/index.html"
+  },
+  {
+    "revision": "bdc6b9344159a272b526",
+    "url": "/robotfriendspwa/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "05f9c680565db1ef6709",
+    "url": "/robotfriendspwa/static/css/main.c49b9767.chunk.css"
+  },
+  {
+    "revision": "bdc6b9344159a272b526",
+    "url": "/robotfriendspwa/static/js/2.5a03802a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robotfriendspwa/static/js/2.5a03802a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f9c680565db1ef6709",
+    "url": "/robotfriendspwa/static/js/main.b2f6f244.chunk.js"
+  },
+  {
+    "revision": "4decbc2ebb99abd0296e",
+    "url": "/robotfriendspwa/static/js/runtime-main.fcd0f640.js"
+  },
+  {
+    "revision": "76a087ccd962305e48648165f86fa40d",
+    "url": "/robotfriendspwa/static/media/Crazy Cartoon.76a087cc.woff"
+  },
+  {
+    "revision": "2c84a11cbaeabb9b2adebec946cbdb56",
+    "url": "/robotfriendspwa/static/media/Spongeboy Me Bob.2c84a11c.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/robotfriendspwa/precache-manifest.83422b87070a20a432ad01eaaac0b8ac.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robotfriendspwa/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
